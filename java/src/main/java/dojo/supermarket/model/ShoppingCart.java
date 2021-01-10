3a00d8ed1ad5a12ea965e85cae688db06358e802
reza ghanbari <rezaqanbari1925@gmail.com>
2021-01-11 00:54:02 +0330

    Remove additional if and variables

--- java/src/main/java/dojo/supermarket/model/ShoppingCart.java
+++ java/src/main/java/dojo/supermarket/model/ShoppingCart.java
@@ -41,32 +41,20 @@ public class ShoppingCart {
                 double unitPrice = catalog.getUnitPrice(p);
                 int quantityAsInt = (int) quantity;
                 Discount discount = null;
-                int x = 1;
-                if (offer.offerType == SpecialOfferType.ThreeForTwo) {
-                    x = 3;
-
-                } else if (offer.offerType == SpecialOfferType.TwoForAmount) {
-                    x = 2;
-                    if (quantityAsInt >= 2) {
-                        double total = offer.argument * (quantityAsInt / x) + quantityAsInt % 2 * unitPrice;
-                        double discountN = unitPrice * quantity - total;
-                        discount = new Discount(p, "2 for " + offer.argument, -discountN);
-                    }
-
-                } if (offer.offerType == SpecialOfferType.FiveForAmount) {
-                    x = 5;
+                if (offer.offerType == SpecialOfferType.TwoForAmount && quantityAsInt >= 2) {
+                    double discountAmount = unitPrice * quantity - offer.argument * (quantityAsInt / 2.0) + quantityAsInt % 2 * unitPrice;
+                    discount = new Discount(p, "2 for " + offer.argument, -discountAmount);
                 }
-                int numberOfXs = quantityAsInt / x;
-                if (offer.offerType == SpecialOfferType.ThreeForTwo && quantityAsInt > 2) {
-                    double discountAmount = quantity * unitPrice - ((numberOfXs * 2 * unitPrice) + quantityAsInt % 3 * unitPrice);
+                if (offer.offerType == SpecialOfferType.ThreeForTwo && quantityAsInt >= 3) {
+                    double discountAmount = quantity * unitPrice - (((quantityAsInt / 3.0) * 2 * unitPrice) + quantityAsInt % 3 * unitPrice);
                     discount = new Discount(p, "3 for 2", -discountAmount);
+                }
+                if (offer.offerType == SpecialOfferType.FiveForAmount && quantityAsInt >= 5) {
+                    double discountTotal = unitPrice * quantity - (offer.argument * (quantityAsInt / 5.0) + quantityAsInt % 5 * unitPrice);
+                    discount = new Discount(p, 5 + " for " + offer.argument, -discountTotal);
                 }
                 if (offer.offerType == SpecialOfferType.TenPercentDiscount) {
                     discount = new Discount(p, offer.argument + "% off", -quantity * unitPrice * offer.argument / 100.0);
-                }
-                if (offer.offerType == SpecialOfferType.FiveForAmount && quantityAsInt >= 5) {
-                    double discountTotal = unitPrice * quantity - (offer.argument * numberOfXs + quantityAsInt % 5 * unitPrice);
-                    discount = new Discount(p, x + " for " + offer.argument, -discountTotal);
                 }
                 if (discount != null)
                     receipt.addDiscount(discount);
