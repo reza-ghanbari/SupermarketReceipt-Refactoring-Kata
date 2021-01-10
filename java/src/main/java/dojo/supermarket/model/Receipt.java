57a49e514e79128a5c3ccd6bae531538fcef34dd
reza ghanbari <rezaqanbari1925@gmail.com>
2021-01-11 03:28:18 +0330

    Add streams for calculating total price of card

--- java/src/main/java/dojo/supermarket/model/Receipt.java
+++ java/src/main/java/dojo/supermarket/model/Receipt.java
@@ -8,14 +8,9 @@ public class Receipt {
     private List<Discount> discounts = new ArrayList<>();
 
     public Double getTotalPrice() {
-        double total = 0.0;
-        for (ReceiptItem item : this.items) {
-            total += item.getTotalPrice();
-        }
-        for (Discount discount : this.discounts) {
-            total -= discount.getDiscountAmount();
-        }
-        return total;
+        double total = this.items.stream().mapToDouble(ReceiptItem::getTotalPrice).sum();
+        double discount = this.discounts.stream().mapToDouble(Discount::getDiscountAmount).sum();
+        return total - discount;
     }
 
     public void addProduct(Product p, double quantity, double price) {
