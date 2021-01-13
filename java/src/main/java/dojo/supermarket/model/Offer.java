4b322cacf29026d6adf6e6ae6db132328a2c953d
mhadih <m.hadi.hojjat@gmail.com>
2021-01-13 23:01:57 +0330

    add bundle feature

--- java/src/main/java/dojo/supermarket/model/Offer.java
+++ java/src/main/java/dojo/supermarket/model/Offer.java
@@ -1,17 +1,31 @@
 package dojo.supermarket.model;
 
+import java.util.HashMap;
+
 public class Offer {
     private final SpecialOfferType offerType;
     private final Product product;
-    private final double argument;
+    private final double amount;
     private String description;
 
-    public Offer(SpecialOfferType offerType, Product product, double argument) {
+    private HashMap<Product, Double> bundleList;
+
+    public Offer(SpecialOfferType offerType, Product product, double amount) {
         this.offerType = offerType;
-        this.argument = argument;
+        this.amount = amount;
         this.product = product;
         setOfferDescription();
     }
+
+    public Offer(SpecialOfferType offerType, HashMap<Product, Double> bundleList) {
+        this.offerType = offerType;
+        this.product = null;
+        this.amount = 0;
+        this.bundleList = bundleList;
+        this.description = "10% off for buy all the bundle";
+    }
+
+    public HashMap<Product, Double> getBundleList() { return bundleList; }
 
     public SpecialOfferType getOfferType() {
         return offerType;
@@ -21,8 +35,8 @@ public class Offer {
         return this.product;
     }
 
-    public double getArgument() {
-        return argument;
+    public double getAmount() {
+        return amount;
     }
 
     public String getDescription() {
@@ -38,10 +52,10 @@ public class Offer {
                 description = "10.0% off";
                 break;
             case TwoForAmount:
-                description = "2 for " + argument;
+                description = "2 for " + amount;
                 break;
             case FiveForAmount:
-                description = "5 for " + argument;
+                description = "5 for " + amount;
                 break;
         }
     }
--- java/src/main/java/dojo/supermarket/model/ShoppingCart.java
+++ java/src/main/java/dojo/supermarket/model/ShoppingCart.java
@@ -19,7 +19,7 @@ public class ShoppingCart {
         this.addItemQuantity(product, 1.0);
     }
 
-    Map<Product, Double> productQuantities() {
+    public Map<Product, Double> getProductQuantities() {
         return productQuantities;
     }
 
@@ -29,37 +29,52 @@ public class ShoppingCart {
         productQuantities.put(product, productQuantities.getOrDefault(product, 0.0) + quantity);
     }
 
-    private double calculateDiscountForAmounts(double quantity, double unitPrice, double argument, int freeItemsPerAmount) {
+    private double calculateDiscountForAmounts(double quantity, double unitPrice, double amount, int freeItemsPerAmount) {
         int roundedQuantity = (int) quantity;
         if (roundedQuantity < freeItemsPerAmount) return 0;
-        return unitPrice * quantity - (argument * (roundedQuantity / freeItemsPerAmount) + roundedQuantity % freeItemsPerAmount * unitPrice);
+        return unitPrice * quantity - (amount * (roundedQuantity / freeItemsPerAmount) + roundedQuantity % freeItemsPerAmount * unitPrice);
     }
 
-    void handleOffers(Receipt receipt, Map<Product, Offer> offers, SupermarketCatalog catalog) {
-        for (Product p: productQuantities().keySet()) {
+    public void handleOffers(Receipt receipt, Map<Product, Offer> offers, SupermarketCatalog catalog) {
+        for (Product p: getProductQuantities().keySet()) {
             if (offers.containsKey(p)) {
                 Offer offer = offers.get(p);
-                double discountAmount = getDiscountAmount(productQuantities.get(p), offer, catalog.getUnitPrice(p));
+                double discountAmount = getDiscountAmount(productQuantities.get(p), offer, catalog.getUnitPrice(p), catalog);
                 if (discountAmount != 0)
                     receipt.addDiscount(new Discount(p, offer.getDescription(), -discountAmount));
             }
         }
     }
 
-    private double getDiscountAmount(double quantity, Offer offer, double unitPrice) {
+    private double getBundleDiscount(HashMap<Product, Double> bundleList, SupermarketCatalog catalog) {
+        int numberInclude = Integer.MAX_VALUE;
+        double totolPrice = 0;
+        for (Product product: bundleList.keySet()) {
+            double productQuantity = productQuantities.getOrDefault(product, 0.0);
+            double bundleQuantity = bundleList.get(product);
+            numberInclude = Math.min(numberInclude, (int)(productQuantity / bundleQuantity));
+            totolPrice += catalog.getUnitPrice(product) * bundleQuantity;
+        }
+        return numberInclude * (0.1) * totolPrice;
+    }
+
+    private double getDiscountAmount(double quantity, Offer offer, double unitPrice, SupermarketCatalog catalog) {
         double discountAmount = 0;
         switch (offer.getOfferType()) {
             case TwoForAmount:
-                discountAmount = calculateDiscountForAmounts(quantity, unitPrice, offer.getArgument(), 2);
+                discountAmount = calculateDiscountForAmounts(quantity, unitPrice, offer.getAmount(), 2);
                 break;
             case ThreeForTwo:
                 discountAmount = calculateDiscountForAmounts(quantity, unitPrice, 2 * unitPrice, 3);
                 break;
             case TenPercentDiscount:
-                discountAmount = quantity * unitPrice * offer.getArgument() / 100.0;
+                discountAmount = quantity * unitPrice * offer.getAmount() / 100.0;
                 break;
             case FiveForAmount:
-                discountAmount = calculateDiscountForAmounts(quantity, unitPrice, offer.getArgument(), 5);
+                discountAmount = calculateDiscountForAmounts(quantity, unitPrice, offer.getAmount(), 5);
+                break;
+            case Bundle:
+                discountAmount = getBundleDiscount(offer.getBundleList(), catalog);
                 break;
         }
         return discountAmount;
